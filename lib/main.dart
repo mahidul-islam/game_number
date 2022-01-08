cdf4686c0ba984ceeb0010abf5d1d6605792e4cc
mahidul-islam <mizihan84@gmail.com>
2022-01-08 19:42:59 +0600

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,122 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+final player = AudioCache(); //Define the player
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return SplashScreen(
+      seconds: 5,
+      navigateAfterSeconds: const AfterSplash(),
+      title: const Text(
+        'my 💕 daughter',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      image: Image.asset(
+        "assets/images/name.gif",
+        // width: MediaQuery.of(context).size.width,
+        // height: 50,
+        fit: BoxFit.cover,
+      ),
+      backgroundColor: Colors.white,
+      styleTextUnderTheLoader: const TextStyle(),
+      photoSize: 100.0,
+      // onClick: () => print("Flutter Egypt"),
+      loaderColor: Colors.red,
+      loadingText: const Text('please wait'),
+      loadingTextPadding: EdgeInsets.zero,
+      useLoader: true,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class AfterSplash extends StatelessWidget {
+  const AfterSplash({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          title: const Text("Kids Game"), automaticallyImplyLeading: false),
+      body: const Center(
+        child: Text(
+          "Done!",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+//     class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder(
+//         future: Future.delayed(Duration(seconds: 3)),
+//         builder: (context, AsyncSnapshot snapshot) {
+//           // Show splash screen while waiting for app resources to load:
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             player.play('sounds/notification_ambient.wav'); //Here is what will play sounds.
+
+//             return MaterialApp(home: Splash());
+//           } else {
+//             // Loading is done, return the app:
+//             return MaterialApp(
+//                 debugShowCheckedModeBanner: false,
+//                 home: TodoApp(),
+//                 title: 'Toodolee',
+//                 theme: ThemeData(
+//                   brightness: Brightness.light,
+//                   fontFamily: "WorkSans",
+//                 ));
+//           }
+//         });
+//   }
+// }
+
+// class Splash extends StatefulWidget {
+//   @override
+//   _SplashState createState() => _SplashState();
+// }
+
+// class _SplashState extends State<Splash> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         color: Colors.white70,
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             FadeIn(
+//               duration: Duration(milliseconds: 1200),
+//               child: Center(
+//                   child: Icon(CarbonIcons.checkmark,
+//                       size: 90, color: Colors.black87)),
+//             ),
+//             FadeOut(
+//                 duration: Duration(milliseconds: 1100),
+//                 child: Center(child: Text("Made by Proco :love"))),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
